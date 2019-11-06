63fc8b304eb415a37fcc222beeed8665a96763a3
Valentyn Malenchak <valentyn17@gmail.com>
2019-11-06 11:50:51 +0200

    <Valentyn Malenchak> Filter view created

--- history_service/models/filter_model.py
+++ history_service/models/filter_model.py
@@ -1,18 +1,15 @@
 from history_service import db
 from sqlalchemy.dialects.postgresql import JSON
-from sqlalchemy.orm import relationship
 
 
 class Filter(db.Model):
     __tablename__ = 'filter'
 
     filter_data = db.Column(JSON, nullable=False)
-    filter_id = db.Column(db.Integer, primary_key=True)
-    history = db.relationship('History', backref='filter_id')
+    filter_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
-    def __init__(self, filter_data, filter_id):
+    def __init__(self, filter_data):
         self.filter_data = filter_data
-        self.filter_id = filter_id
 
     def __repr__(self):
         return f'filter data{self.filter_data}-filter id{self.filter_id}'
--- history_service/serializers/filter_serializer.py
+++ history_service/serializers/filter_serializer.py
@@ -4,4 +4,3 @@ from marshmallow import Schema, fields
 class FilterSchema(Schema):
     filter_data = fields.Raw(required=True)
     filter_id = fields.Integer(dump_only=True)
-
