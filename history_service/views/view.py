a9a510970435fdc92734cefaf79ad639e6cd5cd2
Valentyn Malenchak <valentyn17@gmail.com>
2019-11-07 01:23:07 +0200

    <Valentyn Malenchak> History get method returns filters

--- history_service/views/view.py
+++ history_service/views/view.py
@@ -1,8 +1,10 @@
 import json
+import requests
 from sqlalchemy import exc
 from flask import jsonify, request
 from flask_restful import Resource
 from flask_api import status
+from werkzeug.exceptions import InternalServerError
 from history_service import db
 from history_service import api
 from history_service.models.history_model import History
@@ -15,7 +17,7 @@ class FilterResource(Resource):
 
     def get(self):
         filter_args = request.args
-        if set(filter_args.keys()) == {'filter_id'}:
+        if set(filter_args.keys()).issubset({'filter_id'}):
             try:
                 filter_data = Filter.query.filter_by(**filter_args).first_or_404()
                 filter_serializer = FilterSchema()
@@ -40,13 +42,26 @@ class FilterResource(Resource):
 
 class HistoryResource(Resource):
 
+    @staticmethod
+    def get_filter_by_history_record(history_record):
+        filter_id = history_record.pop('filter_id')
+        filter = requests.get(f'http://localhost:5000/filter?filter_id={filter_id}')
+        if filter.status_code == 200:
+            history_record.update(filter.json())
+        else:
+            raise InternalServerError
+
     def get(self):
         request_args = request.args
-        if set(request_args.keys()).issubset({'user_id', 'file_id', 'filter'}):
+        if set(request_args.keys()).issubset({'user_id', 'file_id', 'filter_id'}):
             try:
                 history_data = History.query.filter_by(**request_args).all()
                 history_serializer = HistorySchema()
-                return jsonify({'history': [history_serializer.dump(record) for record in history_data]})
+                dumped_history = [history_serializer.dump(record) for record in history_data]
+                if dumped_history:
+                    for record in dumped_history:
+                        self.get_filter_by_history_record(record)
+                return jsonify({'history': dumped_history})
             except exc.SQLAlchemyError:
                 return status.HTTP_400_BAD_REQUEST
         return status.HTTP_400_BAD_REQUEST
