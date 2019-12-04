0359871aab8629bfdeefc5037b44f5a4b313b187
Valentyn Malenchak <valentyn17@gmail.com>
2019-12-04 10:53:43 +0200

    <Valentyn Malenchak> tests for SingleFilterResource added

--- tests/tests_for_filter_view.py
+++ tests/tests_for_filter_view.py
@@ -1,8 +1,10 @@
 """Module for testing filter view."""
 import unittest
+import json
 from history_service import APP, DB
 from history_service.views.filter_view import FiltersResource,\
                                               SingleFilterResource
+from history_service.views.history_view import HistoryResource
 
 
 class HistoryServiceTestCase(unittest.TestCase):
@@ -19,5 +21,16 @@ class FiltersResourceTestCase(HistoryServiceTestCase):
 
     def test_filters_getting(self):
         response = self.APP.get('/filter')
-        print(response.status_code)
         self.assertEqual(response.status_code, 200)
+
+
+class SingleFilterResourceTestCase(HistoryServiceTestCase):
+
+    def test_correct_filter_getting(self):
+        with open('tests/request_files/history_record_create.json', 'r') as history_json:
+            history_content = json.loads(history_json.read())
+            response = self.APP.post('/history', json=history_content)
+
+        self.assertEqual(response.status_code, 201)
+
+
